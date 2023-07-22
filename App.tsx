a98af906ae089ec090922e7ce680569729b17c80
Rohit Kumar <rohit96682@gmail.com>
2023-07-22 10:57:33 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <View>
-      <Text style={{color: 'red', fontSize: 30}}>This is text component</Text>
-      <Button title="Press Here" />
+      <Text style={{color: 'red', fontSize: 30}}>App component</Text>
+      <Home />
     </View>
   );
 }
---
+++ components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text style={{fontSize: 30, color: 'green'}}>Home Component</Text>
+    </View>
+  );
+}
+
+export default Home;
