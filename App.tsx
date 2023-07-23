06e25e25382432fe53f6f69e8e1882e359fee587
Rohit Kumar <rohit96682@gmail.com>
2023-07-23 17:58:29 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 const users = [
   {
@@ -17,22 +17,112 @@ const users = [
     name: 'Lalit Kumar',
     email: 'lalit@gmail.com',
   },
+  {
+    id: 11,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 12,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 13,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 21,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 22,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 23,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 31,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 32,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 33,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 41,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 42,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 43,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 51,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 52,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 53,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
 ];
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Flat List</Text>
-      <FlatList
-        data={users}
-        renderItem={({item}) => <Text>{item.name}</Text>}
-      />
+      <ScrollView>
+        {users.map(item => {
+          return <Text style={styles.text}>{item.name}</Text>;
+        })}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+
   title: {
     fontSize: 30,
+    marginBottom: 15,
+  },
+
+  text: {
+    padding: 10,
+    marginBottom: 10,
+    fontSize: 20,
+    backgroundColor: '#FFB6C1',
+    color: 'white',
   },
 });
