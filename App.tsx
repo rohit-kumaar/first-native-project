7a9c21053badaa5471e0e0d412aba5cfc6f921c4
Rohit Kumar <rohit96682@gmail.com>
2023-07-23 18:01:44 +0530

    more json data, and styling list

--- App.tsx
+++ App.tsx
@@ -17,22 +17,111 @@ const users = [
     name: 'Lalit Kumar',
     email: 'lalit@gmail.com',
   },
+  {
+    id: 11,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 12,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 13,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 21,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 22,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 23,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 31,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 32,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 33,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 41,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 42,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 43,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
+  {
+    id: 51,
+    name: 'Rohit Kumar',
+    email: 'rohit@gmail.com',
+  },
+  {
+    id: 52,
+    name: 'Bibhu Prasad',
+    email: 'bibhu@gmail.com',
+  },
+  {
+    id: 53,
+    name: 'Lalit Kumar',
+    email: 'lalit@gmail.com',
+  },
 ];
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Flat List</Text>
       <FlatList
         data={users}
-        renderItem={({item}) => <Text>{item.name}</Text>}
+        renderItem={({item}) => <Text style={styles.text}>{item.name}</Text>}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+
   title: {
     fontSize: 30,
+    marginBottom: 15,
+  },
+
+  text: {
+    padding: 10,
+    marginBottom: 10,
+    fontSize: 20,
+    backgroundColor: '#FFB6C1',
+    color: 'white',
   },
 });
