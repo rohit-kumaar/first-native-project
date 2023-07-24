94612c317ea3d71099824ce42c64546a7c485fdd
Rohit Kumar <rohit96682@gmail.com>
2023-07-24 23:19:39 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,66 +1,25 @@
-import React from 'react';
-import {SectionList, StyleSheet, Text, View} from 'react-native';
-
-const users = [
-  {
-    id: 1,
-    name: 'Rohit Kumar',
-    data: ['Hamirpur', 'EEE', 'IT'],
-  },
-  {
-    id: 2,
-    name: 'Bibhu Prasad',
-    data: ['Koelnagar', 'EEE', 'IT'],
-  },
-  {
-    id: 3,
-    name: 'Lalit Kumar',
-    data: ['Sec-19', 'EEE', 'IT'],
-  },
-];
+import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Section List</Text>
+  const [count, setCount] = useState(0);
 
-      <SectionList
-        sections={users}
-        renderItem={({item}) => (
-          <View style={styles.user_wrapper}>
-            <Text>{item}</Text>
-          </View>
-        )}
-        renderSectionHeader={({section: {name}}) => (
-          <Text style={styles.title}>{name}</Text>
-        )}
-      />
+  useEffect(() => {
+    console.warn(`This useEffect "ComponentDidMount" only once`);
+  }, []);
+
+  return (
+    <View>
+      <Text style={styles.text}>{count}</Text>
+      <Button title="update count" onPress={() => setCount(count + 1)} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 15,
-  },
-
-  user_wrapper: {
-    flexDirection: 'row',
-    gap: 10,
-  },
-
-  title: {
-    fontSize: 30,
-    marginBottom: 15,
-  },
-
   text: {
-    flex: 1,
-    padding: 10,
-    marginBottom: 10,
-    fontSize: 20,
-    backgroundColor: '#3F2B96',
-    color: 'white',
+    fontSize: 30,
+    marginVertical: 10,
+    textAlign: 'center',
   },
 });
