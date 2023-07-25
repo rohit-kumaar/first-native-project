3b29b508077556beae1da8ae7f291bf038358a54
Rohit Kumar <rohit96682@gmail.com>
2023-07-25 22:52:17 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,68 @@
-import {View, Text, Button, StyleSheet} from 'react-native';
 import React, {useEffect, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(10);
 
   useEffect(() => {
-    console.warn(`This useEffect "ComponentDidMount" only once`);
-  }, []);
+    // console.warn('This useEffect will run when count is update');
+  }, [count]);
+
+  useEffect(() => {
+    // console.warn('This useEffect will run when data is update');
+  }, [data]);
 
   return (
     <View>
-      <Text style={styles.text}>{count}</Text>
-      <Button title="update count" onPress={() => setCount(count + 1)} />
+      <Text
+        style={{fontSize: 30, textAlign: 'center', textTransform: 'uppercase'}}>
+        Component Did Update
+      </Text>
+
+      <View>
+        <Text>Print count : {count}</Text>
+        <Button title="Update count" onPress={() => setCount(count + 1)} />
+      </View>
+
+      <View>
+        <Text>Print data : {data}</Text>
+        <Button title="Update count" onPress={() => setData(data + 1)} />
+      </View>
+
+      <View>
+        <Text
+          style={{
+            fontSize: 30,
+            textAlign: 'center',
+            textTransform: 'uppercase',
+          }}>
+          Props Data
+        </Text>
+        <User info={{count, data}} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    marginVertical: 10,
-    textAlign: 'center',
-  },
-});
+function User(props: any) {
+  useEffect(() => {
+    console.warn('This useEffect will run when count props is update');
+  }, [props.info.count]);
+
+  useEffect(() => {
+    console.warn('This useEffect will run when data props is update');
+  }, [props.info.data]);
+
+  return (
+    <View>
+      <View>
+        <Text>Print count : {props.info.count}</Text>
+      </View>
+
+      <View>
+        <Text>Print data : {props.info.data}</Text>
+      </View>
+    </View>
+  );
+}
