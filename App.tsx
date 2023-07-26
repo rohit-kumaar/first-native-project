1845c2e997c18dd5f110305978513fb085db1cdf
Rohit Kumar <rohit96682@gmail.com>
2023-07-26 23:03:31 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,68 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(10);
-
-  useEffect(() => {
-    // console.warn('This useEffect will run when count is update');
-  }, [count]);
-
-  useEffect(() => {
-    // console.warn('This useEffect will run when data is update');
-  }, [data]);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <View>
-      <Text
-        style={{fontSize: 30, textAlign: 'center', textTransform: 'uppercase'}}>
-        Component Did Update
-      </Text>
-
-      <View>
-        <Text>Print count : {count}</Text>
-        <Button title="Update count" onPress={() => setCount(count + 1)} />
-      </View>
-
-      <View>
-        <Text>Print data : {data}</Text>
-        <Button title="Update count" onPress={() => setData(data + 1)} />
-      </View>
-
-      <View>
-        <Text
-          style={{
-            fontSize: 30,
-            textAlign: 'center',
-            textTransform: 'uppercase',
-          }}>
-          Props Data
-        </Text>
-        <User info={{count, data}} />
-      </View>
+      <Button
+        title={`Toggle is : ${toggle ? 'Show' : 'Hide'}`}
+        onPress={() => setToggle(!toggle)}
+      />
+      {toggle ? <User /> : null}
     </View>
   );
 }
 
-function User(props: any) {
-  useEffect(() => {
-    console.warn('This useEffect will run when count props is update');
-  }, [props.info.count]);
-
-  useEffect(() => {
-    console.warn('This useEffect will run when data props is update');
-  }, [props.info.data]);
-
-  return (
-    <View>
-      <View>
-        <Text>Print count : {props.info.count}</Text>
-      </View>
-
-      <View>
-        <Text>Print data : {props.info.data}</Text>
-      </View>
-    </View>
-  );
+function User() {
+  return <Text>User Component</Text>;
 }
