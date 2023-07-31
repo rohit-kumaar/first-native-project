e577bf11de1b25eef3c6f1fd4801232fbdfb1916
Rohit Kumar <rohit96682@gmail.com>
2023-07-31 22:43:31 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
-const myImg = require('./assets/rohit.jpeg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Image source={myImg} style={styles.myImg} />
-      <Image source={{uri: 'https://picsum.photos/300'}} style={styles.myImg} /> */}
-
-      <ImageBackground source={myImg} style={{flex: 1}}>
-        <Text>Rohit Kumar</Text>
-      </ImageBackground>
+      <View style={[styles.child_1]}>
+        <View style={[styles.child_4]}></View>
+        <View style={[styles.child_5]}></View>
+        <View style={[styles.child_6]}></View>
+      </View>
+      <View style={[styles.child, styles.child_2]}></View>
+      <View style={[styles.child, styles.child_3]}></View>
     </View>
   );
 }
@@ -18,12 +18,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: 'plum',
   },
 
-  myImg: {
-    width: 300,
-    height: 300,
+  child: {
+    flex: 1,
+  },
+
+  child_1: {
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#008080',
+  },
+
+  child_2: {
+    backgroundColor: '#FFA500',
+  },
+
+  child_3: {
+    backgroundColor: '#0000FF',
+  },
+
+  child_4: {
+    flex: 1,
+    backgroundColor: '#3A4A7D',
+  },
+
+  child_5: {
+    flex: 1,
+    backgroundColor: '#F73859',
+  },
+
+  child_6: {
+    flex: 1,
+    backgroundColor: '#1e2b4f',
   },
 });
