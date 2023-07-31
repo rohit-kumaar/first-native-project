12c6dd28ac2b40283f43863c9760c2c5212190f7
Rohit Kumar <rohit96682@gmail.com>
2023-07-31 23:01:42 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,56 +1,24 @@
-import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={[styles.child_1]}>
-        <View style={[styles.child_4]}></View>
-        <View style={[styles.child_5]}></View>
-        <View style={[styles.child_6]}></View>
-      </View>
-      <View style={[styles.child, styles.child_2]}></View>
-      <View style={[styles.child, styles.child_3]}></View>
-    </View>
+    <TouchableHighlight>
+      <Text style={styles.primary}>Primary</Text>
+    </TouchableHighlight>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'plum',
-  },
-
-  child: {
-    flex: 1,
-  },
-
-  child_1: {
-    flex: 2,
-    flexDirection: 'row',
-    backgroundColor: '#008080',
-  },
-
-  child_2: {
-    backgroundColor: '#FFA500',
-  },
-
-  child_3: {
-    backgroundColor: '#0000FF',
-  },
-
-  child_4: {
-    flex: 1,
-    backgroundColor: '#3A4A7D',
-  },
-
-  child_5: {
-    flex: 1,
-    backgroundColor: '#F73859',
-  },
-
-  child_6: {
-    flex: 1,
-    backgroundColor: '#1e2b4f',
+  primary: {
+    paddingVertical: 12,
+    textAlign: 'center',
+    fontSize: 24,
+    backgroundColor: '#0B5ED7',
+    color: 'white',
+    borderRadius: 10,
+    margin: 10,
+    shadowColor: 'black',
+    elevation: 10,
   },
 });
