fb3cf88440cbc1501059c7cb1f5f07f2889fb28d
Rohit Kumar <rohit96682@gmail.com>
2023-08-08 23:00:28 +0530

    final commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,18 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
 export default function App() {
   const [data, setData] = useState([]);
 
-  const API_URL = 'https://jsonplaceholder.typicode.com/users';
+  const API_URL = 'http://10.0.2.2:3000/users';
 
   async function apiData() {
     let res = await fetch(API_URL);
@@ -16,16 +24,38 @@ export default function App() {
     apiData();
   }, []);
 
+  const deleteUser = id => {
+    axios
+      .delete(`${API_URL}/${id}`)
+      .then(() => {
+        apiData();
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  };
+
   return (
     <ScrollView style={styles.container}>
       {data.length ? (
         <FlatList
           data={data}
           renderItem={({item}) => (
-            <View style={styles.text}>
+            <View style={styles.textWrapper}>
               <Text>User ID : {item.id}</Text>
               <Text>User Name : {item.name}</Text>
               <Text>User Email : {item.email}</Text>
+
+              <TouchableHighlight>
+                <Text style={[styles.btn, styles.success]}>Update</Text>
+              </TouchableHighlight>
+              <TouchableHighlight>
+                <Text
+                  style={[styles.btn, styles.danger]}
+                  onPress={() => deleteUser(item.id)}>
+                  Delete
+                </Text>
+              </TouchableHighlight>
             </View>
           )}
         />
@@ -46,11 +76,34 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
 
-  text: {
+  textWrapper: {
     padding: 10,
     marginBottom: 10,
     fontSize: 20,
     backgroundColor: '#FFB6C1',
     color: 'white',
+  },
+
+  btnWrapper: {
+    flexDirection: 'row',
+  },
+
+  btn: {
+    paddingVertical: 8,
+    textAlign: 'center',
+    fontSize: 20,
+    color: 'white',
+    borderRadius: 10,
+    margin: 10,
+    shadowColor: 'black',
+    elevation: 10,
+  },
+
+  success: {
+    backgroundColor: '#198754',
+  },
+
+  danger: {
+    backgroundColor: '#dc3545',
   },
 });
